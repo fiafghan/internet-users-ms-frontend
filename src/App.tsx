0d5a87a12a18b42118630202c117f53358c273f4
fiafghan <fiafghan@gmail.com>
2025-07-12 11:53:31 +0430

    Login page was added!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import RegisterForm from "./RegisterForm"
 import { Routes, Route} from "react-router-dom"
+import LoginForm from "./LoginForm"
+
+
 function App() {
 
   return (
     <>
       <Routes>
       <Route path="/register" element={<RegisterForm />} />
+      <Route path="/login" element={<LoginForm />} />
     </Routes>
     </>
   )
