4fa5f2120f462369cf519f2c6158d70584316dc3
fiafghan <fiafghan@gmail.com>
2025-07-16 15:51:38 +0430

    the form of violation was completed but styling was not that much beautiful!

--- src/AddViolation.tsx
+++ src/AddViolation.tsx
@@ -169,20 +169,12 @@ export default function AddViolation() {
       </tbody>
     </table>
   )}
-
-  {selectedUser && (
-    <div className="flex justify-end mt-6">
-      <button
-        type="submit"
-        className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-500 transition-all"
-      >
-        ثبت تخلف
-      </button>
-    </div>
-  )}
 </form>
-
-
+      <div className="grid grid-cols-1">
+        <h1 className="ml-100 mr-50 mt-5" dir = "rtl">کاربران انترنت در وزارت صحت عامه در صورتیکه دو مرتبه از انترنت استفاده نادرست نمایند یوزر شان برای همیشه مسدود می گردد!</h1>
+        <h1 className="mx-auto mt-5" dir = "rtl">برای همیشه مسدود میگردد</h1>
+        <h1 className="mx-auto mt-5" dir = "rtl">امضا رییس تکنالوژی معلوماتی</h1>
+      </div>
       </main>
     </div>
   );
